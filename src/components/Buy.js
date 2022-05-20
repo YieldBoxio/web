a53344316548bec152b0fb0821ae074feb1bff42
technoliDeveloper <yieldboxofficial@gmail.com>
2022-05-20 18:56:52 +0500

    Update buy content and enjoy buttons

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -24,8 +24,15 @@ const Buy = () => {
                   <Card className="text-center text-white p-4">
                     <div className="card__content">
                       <img alt="card img" width={80} src={download} />
-                      <h5>Download and setup MetaMask or TrustWallet</h5>
-                      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantiu m doloremque laudantium, totam rem aperiam, eaque ipsa. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantiu m doloremque laudantium, totam rem aperiam, eaque ipsa.</p>
+                      <h5>Download & setup MetaMask or TrustWallet</h5>
+                      <p>
+                        Download MetaMask (a crypto wallet in the form of a browser extension) or TrustWallet (an app for
+                        your phone). After that you will have to add the Binance Smart Chain to your network-list.
+                      </p>
+                      <div className="next-move-give">
+                        <a href="">MetaMask: Fast Guide Here</a>
+                        <a href="">TrustWallet: Fast Guide Here</a>
+                      </div>
                     </div>
                   </Card>
                 </Slide>
@@ -35,8 +42,8 @@ const Buy = () => {
                   <Card className="text-center text-white p-4">
                     <div className="card__content">
                       <img alt="card img" width={80} src={wallet} />
-                      <h5>Buy and Send BNB to your Wallet</h5>
-                      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantiu m doloremque laudantium, totam rem aperiam, eaque ipsa. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantiu m doloremque laudantium, totam rem aperiam, eaque ipsa.</p>
+                      <h5>Buy and send BNB to your Wallet</h5>
+                      <p>Buy BNB on an exchange (i.e. Binance, Kraken, Coinbase etc.). Transfer the tokens to your MetaMask wallet address. BEP-20 addresses start with a “0x”.</p>
                     </div>
                   </Card>
                 </Slide>
@@ -46,8 +53,14 @@ const Buy = () => {
                   <Card className="text-center text-white p-4">
                     <div className="card__content">
                       <img alt="card img" width={80} src={sgc} />
-                      <h5>Go to PanecakeSwap and swap for $SGC</h5>
-                      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantiu m doloremque laudantium, totam rem aperiam, eaque ipsa. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantiu m doloremque laudantium, totam rem aperiam, eaque ipsa.</p>
+                      <h5>Go to PancakeSwap and swap for $YBX</h5>
+                      <p>
+                        Click here to head on over to PancakeSwap or use this address TBA to select YieldBox. Set the
+                        slippage tolerance to 11% (sometimes it may be 15-18%, depending on how much demand there is).
+                      </p>
+                      <div className="next-move-give">
+                        <a href="">PancakeSwap Buy link: TBA</a>
+                      </div>
                     </div>
                   </Card>
                 </Slide>
