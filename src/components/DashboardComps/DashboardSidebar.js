fb45c7ddafb8a6f06e7b989eeffa9c1210f7e3d6
MAwaisme <73178485+MAwaisme@users.noreply.github.com>
2022-05-30 14:11:57 +0500

    dashboard complete UI

--- src/components/DashboardComps/DashboardSidebar.js
+++ src/components/DashboardComps/DashboardSidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/img/logo.svg";
@@ -7,9 +7,16 @@ import Calculator from "../../assets/img/calculator-link-img.png";
 import Swap from "../../assets/img/swap-link-img.png";
 import Docs from "../../assets/img/docs-link-img.png";
 import Slide from "react-reveal/Slide";
+import { useNavigate } from "react-router-dom";
 
 const DashboardSidebar = () => {
+    let navigate = useNavigate();
+    console.log("🚀 ~ file: DashboardSidebar.js ~ line 14 ~ DashboardSidebar ~ navigate", navigate)
 
+    const [active, setActive] = useState(false);
+    const clickHandle = () => {
+        setActive(true)
+    }
     return (
         <Container className='p-0'>
             <Slide left>
@@ -26,19 +33,24 @@ const DashboardSidebar = () => {
             </Slide>
             <Nav className="ml-auto dash-nav-links-wrapper">
                 <Slide left>
-                    <NavLink to="/dashboard" className="nav-link px-3 dash-link active-dash-link">
-                        <img src={Dashlinkimg} /> Dashboard
-                    </NavLink>
-                    <NavLink to="/dashboard" className="nav-link px-3 dash-link">
+                    {/* <NavLink to="/dashboard" className={ navigate == '/dashboard' ? "nav-link px-3 dash-link active-dash-link" : "nav-link px-3 dash-link"}>
+                    </NavLink> */}
+                    <div onClick={() =>clickHandle()} className={ active ? "nav-link px-3 dash-link active-dash-link" : "nav-link px-3 dash-link"}>
+                         <img src={Dashlinkimg} /> Dashboard
+                    </div>
+                    <div onClick={() =>clickHandle()} className={ active ? "nav-link px-3 dash-link active-dash-link" : "nav-link px-3 dash-link"}>
+                         <img src={Dashlinkimg} />  Claim Reward
+                    </div>
+                    {/* <NavLink to="/claim" className={'nav-link px-3 dash-link'}>
                         <img src={Dashlinkimg} /> Claim Reward
-                    </NavLink>
-                    <NavLink to="/dashboard" className="nav-link px-3 dash-link">
+                    </NavLink> */}
+                    <NavLink to="/dashboardmainview/calculator" className={'nav-link px-3 dash-link'}>
                         <img src={Calculator} /> Calculator
                     </NavLink>
-                    <NavLink to="/dashboard" className="nav-link px-3 dash-link">
+                    <NavLink to="/swap" className={'nav-link px-3 dash-link'}>
                         <img src={Swap} /> Swap
                     </NavLink>
-                    <NavLink to="/dashboard" className="nav-link px-3 dash-link">
+                    <NavLink to="/" className={'nav-link px-3 dash-link'}>
                         <img src={Docs} /> Docs
                     </NavLink>
                 </Slide>
