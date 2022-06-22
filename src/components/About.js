dd6064a8944ae6f05b5705e92a2be5877fc0e321
YieldBox <104127072+YieldBoxio@users.noreply.github.com>
2022-06-22 13:39:01 +0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -11,8 +11,9 @@ const About = () => {
             <div className="col-xl-12 col-lg-12 col-sm-10 text-center about__content">
               <Container>
                 <p>
-                  “World’s No.1 Yield Box Auto-Staking and Auto-Reflection Protocol (YASAR) with every
-                  5-minute Reward and every hour for 7% <span className="BUSD__color">BUSD</span> Reflection”
+                  “World’s No.1 Yield Box Auto-Staking and Auto-Reflection
+                  Protocol (YASAR) with every 5-minute Reward and every hour for
+                  7% <span className="BUSD__color">BUSD</span> Reflection”
                 </p>
               </Container>
               <Row className="justify-content-center m-0 w-md-75">
@@ -22,9 +23,11 @@ const About = () => {
                   </Button>
                 </Col>
                 <Col lg="4" md="6" sm="8" className="my-3">
-                  <Button className="btn-gradient btn-block btn-lg btn">
-                    Read Whitepaper
-                  </Button>
+                  <a href="https://yieldboxio.gitbook.io" target={"_blank"}>
+                    <Button className="btn-gradient btn-block btn-lg btn">
+                      Read Whitepaper
+                    </Button>
+                  </a>
                 </Col>
               </Row>
             </div>
